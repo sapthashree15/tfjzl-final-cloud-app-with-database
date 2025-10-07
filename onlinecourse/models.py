c5507e488de8ee3bb2c8ff2c486328372db9389f
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-07 18:48:40 +0530

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -106,13 +106,6 @@ class Question(models.Model):
         return all_correct.issubset(selected)
 
 
-# ✅ Choice model
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=100)
-    is_correct = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.choice_text
 
 
