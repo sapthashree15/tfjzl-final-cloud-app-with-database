2d0d9a9c0a63953498b946116a1c785cad3c890d
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-07 09:51:11 +0530

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -116,10 +116,4 @@ class Choice(models.Model):
         return self.choice_text
 
 
-# ✅ Submission model
-class Submission(models.Model):
-    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-    choices = models.ManyToManyField(Choice)
 
-    def __str__(self):
-        return f"Submission {self.id} for enrollment {self.enrollment.id}"
