a5f01a375420c0b0a34341bcd89ab5e490b24f5c
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-07 18:28:23 +0530

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -14,10 +14,6 @@ class QuestionInline(admin.StackedInline):
 
 # <HINT> Register Question and Choice models here
 
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
-admin.site.register(Learner)
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
-admin.site.register(Submission)
