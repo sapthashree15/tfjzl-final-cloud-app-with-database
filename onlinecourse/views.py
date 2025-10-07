d36dc93e6a697d589f61136ef3263621a8cf5d78
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-07 16:06:17 +0530

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -134,3 +134,28 @@ def submit(request, course_id):
     return HttpResponseRedirect(
         reverse('onlinecourse:show_exam_result', args=(course_id, submission.id))
     )
+
+
+# Show exam result view
+def show_exam_result(request, course_id, submission_id):
+    course = get_object_or_404(Course, pk=course_id)
+    submission = get_object_or_404(Submission, pk=submission_id)
+
+    selected_ids = submission.choices.values_list('id', flat=True)
+
+    total_score = 0
+    possible_score = 0
+
+    questions = course.question_set.all()
+    for question in questions:
+        possible_score += question.grade
+        if question.is_get_score(selected_ids):
+            total_score += question.grade
+
+    context = {
+        'course': course,
+        'selected_ids': selected_ids,
+        'grade': total_score,
+        'possible': possible_score,
+    }
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
