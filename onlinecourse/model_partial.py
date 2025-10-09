a66ac13f20b9ea10c5be545b4c070b6b0606444e
VandanaPande <129248223+VandanaPande@users.noreply.github.com>
2025-10-09 13:08:43 +0530

    Update model_partial.py

--- onlinecourse/model_partial.py
+++ onlinecourse/model_partial.py
@@ -90,22 +90,6 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
-# ✅ Question model
-class Question(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    question_text = models.TextField()
-    grade = models.IntegerField(default=1)
-
-    def __str__(self):
-        return self.question_text
-
-    # Method to check if learner got the score
-    def is_get_score(self, selected_choice_ids):
-        all_correct = set(self.choice_set.filter(is_correct=True).values_list('id', flat=True))
-        selected = set(selected_choice_ids)
-        return all_correct.issubset(selected)
-
-
 
 
 # ✅ Submission model
