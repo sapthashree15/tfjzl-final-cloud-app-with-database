1bd8bd06fd2d26bce9baf71c903b0444152c3f1b
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-16 09:35:01 +0530

    Add fields to Choice model and fix string formatting

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -109,7 +109,9 @@ class Question(models.Model):
 # ✅ Choice model
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-   
+    choice_text = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
     def __str__(self):
         return self.choice_text
 
