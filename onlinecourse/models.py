d287944782f99ac56a035aa3da9f147e03d2cd24
Sameep-Skillup <sameep.punjani@skillup.online>
2025-10-16 12:26:28 +0530

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -92,8 +92,12 @@ class Enrollment(models.Model):
 
 # ✅ Question model
 class Question(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+        course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    grade = models.IntegerField(default=1)
 
+    def __str__(self):
+        return self.question_text
 
     # Method to check if learner got the score
     def is_get_score(self, selected_choice_ids):
@@ -102,15 +106,18 @@ class Question(models.Model):
         return all_correct.issubset(selected)
 
 
+
 # ✅ Choice model
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
     
 
 
 # ✅ Submission model
 class Submission(models.Model):
-   
-
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
     def __str__(self):
         return f"Submission {self.id} for enrollment {self.enrollment.id}"
