7cb17fe479c74d707ac5fd27483d1ebcb7ca05a3
sapthashree15 <96560683+sapthashree15@users.noreply.github.com>
2025-10-29 16:28:50 +0530

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -90,6 +90,21 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 
+# ✅ Question model
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    grade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.question_text
+
+    # Method to check if learner got the score
+    def is_get_score(self, selected_choice_ids):
+        all_correct = set(self.choice_set.filter(is_correct=True).values_list('id', flat=True))
+        selected = set(selected_choice_ids)
+        return all_correct.issubset(selected)
+
 
 # ✅ Choice model
 class Choice(models.Model):
